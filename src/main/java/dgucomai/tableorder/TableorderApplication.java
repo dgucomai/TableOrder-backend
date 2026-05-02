d7407e68402e5966b784888e37b27c520b42a941
Secuho <secuho@secuho.life>
2026-05-02 14:50:14 +0900

    [INIT]: Project Initializing.

---
+++ src/main/java/dgucomai/tableorder/TableorderApplication.java
@@ -0,0 +1,13 @@
+package dgucomai.tableorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dgucomai/tableorder/TableorderApplicationTests.java
@@ -0,0 +1,13 @@
+package dgucomai.tableorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
