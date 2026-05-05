c58a6429baa2d5ac0b83e0b3a52a638cd70575f5
Secuho <secuho@secuho.life>
2026-05-05 10:23:53 +0900

    [FEAT] Temporary Add. (Nginx Test Controller)

--- src/main/java/dgucomai/tableorder/controller/ServerTestController.java
+++ src/main/java/dgucomai/tableorder/controller/ServerTestController.java
@@ -1,17 +1,15 @@
 package dgucomai.tableorder.controller;
 
+import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
 
 @RestController
 public class ServerTestController {
 
-    @GetMapping("/api/test")
-    public String getServerTime() {
-        LocalDateTime now = LocalDateTime.now();
-        return "현재 서버 시간 : " + now;
-    }
-
+  @GetMapping("/api/test")
+  public String getServerTime() {
+    LocalDateTime now = LocalDateTime.now();
+    return "현재 서버 시간 : " + now;
+  }
 }
