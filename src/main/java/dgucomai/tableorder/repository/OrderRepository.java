a92c75d76f8aae072ab2d8fc30a0d1b6d16db076
Jeongbin Cho/조정빈 <165370817+wjdbin@users.noreply.github.com>
2026-05-07 01:07:00 +0900

    feat: 주문 승인 및 거절 기능 구현 및 API 설계 (#9)

--- src/main/java/dgucomai/tableorder/repository/OrderRepository.java
+++ src/main/java/dgucomai/tableorder/repository/OrderRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    // 이 한 줄만으로 DB에서 주문을 찾고, 저장하고, 삭제하는 모든 기능이 완성됩니다!
+
 }
