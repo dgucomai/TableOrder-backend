1c11907784c4b2959452f613da268fa5302b2be3
Jibbitz404 <minjun8707@gmail.com>
2026-05-19 09:10:18 +0900

    fix: 게임 사용자 수정 API 점수 필드명 오류 수정

--- src/main/java/dgucomai/tableorder/dto/GameUserUpdateReqDto.java
+++ src/main/java/dgucomai/tableorder/dto/GameUserUpdateReqDto.java
@@ -3,28 +3,20 @@ package dgucomai.tableorder.dto;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 public record GameUserUpdateReqDto(
-        String nickname,
-        String phoneNumber,
-
-        @JsonAlias({"score_1", "flappy_bird_score", "flappyBirdScore"})
-        Integer flappyBirdScore,
-
-        @JsonAlias({"score_2", "rock_paper_scissors_score", "rockPaperScissorsScore"})
+    String nickname,
+    String phoneNumber,
+    @JsonAlias({"score_1", "flappy_bird_score", "flappyBirdScore"}) Integer flappyBirdScore,
+    @JsonAlias({"score_2", "rock_paper_scissors_score", "rockPaperScissorsScore"})
         Integer rockPaperScissorsScore,
-
-        @JsonAlias({"score_3", "ako_growing_score", "akoGrowingScore"})
-        Integer akoGrowingScore,
-
-        @JsonAlias({"score_4", "basketball_score", "basketballScore"})
-        Integer basketballScore
-) {
+    @JsonAlias({"score_3", "ako_growing_score", "akoGrowingScore"}) Integer akoGrowingScore,
+    @JsonAlias({"score_4", "basketball_score", "basketballScore"}) Integer basketballScore) {
 
   public boolean hasNoUpdateFields() {
     return nickname == null
-            && phoneNumber == null
-            && flappyBirdScore == null
-            && rockPaperScissorsScore == null
-            && akoGrowingScore == null
-            && basketballScore == null;
+        && phoneNumber == null
+        && flappyBirdScore == null
+        && rockPaperScissorsScore == null
+        && akoGrowingScore == null
+        && basketballScore == null;
   }
 }
