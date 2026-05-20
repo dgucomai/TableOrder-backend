31d8a66436f2f3fd6f49120c577a74a517c56773
top6543top <top6543top@gamil.com>
2026-05-20 23:54:36 +0900

    fix : status 이름 불일치에 따른 오류 수정

--- src/main/java/dgucomai/tableorder/repository/OrdersRepository.java
+++ src/main/java/dgucomai/tableorder/repository/OrdersRepository.java
@@ -1,14 +1,14 @@
 package dgucomai.tableorder.repository;
 
 import dgucomai.tableorder.domain.entity.Orders;
-import dgucomai.tableorder.domain.enums.PaymentStatus;
+import dgucomai.tableorder.domain.enums.OrderStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
-  boolean existsBySessionIdAndPaymentStatus(Long sessionId, PaymentStatus paymentStatus);
+  boolean existsBySessionIdAndOrderStatus(Long sessionId, OrderStatus orderStatus);
 
   List<Orders> findBySessionId(Long sessionId);
 }
